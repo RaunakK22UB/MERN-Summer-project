c3c2976a48c5f0b3cd33749e5ed17fb788dbacd3
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-06-17 14:58:33 +0530

    Initial commit

---
+++ src/Login.js
@@ -0,0 +1,41 @@
+
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+ 
+  };
+
+  return (
+    <div >
+      <h2>Login Form</h2>
+      <form onSubmit={handleLogin} >
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          
+          required
+        />
+        <button type="submit" >Login</button>
+      </form>
+    </div>
+  );
+}
+
+
+
+export default Login;
